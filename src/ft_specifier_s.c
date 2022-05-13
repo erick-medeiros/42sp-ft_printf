dc9d9f77755104d96ac5b90bccb257c8c797d3e1
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-05-13 23:40:13 +0200

    fix: ft_strdup(NULL)

--- src/ft_specifier_s.c
+++ src/ft_specifier_s.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 00:23:47 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/05/13 16:55:18 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/05/13 23:38:07 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,24 @@
 
 void	ft_specifier_s(t_format *fmt, t_holder *hdr)
 {
-	char	*s;
-	char	*sjoin;
+	char		*s;
+	char		*sjoin;
+	size_t	len;
 
-	s = ft_strdup(va_arg(fmt->ap, char *));
+	s = va_arg(fmt->ap, char *);
 	if (s == NULL)
 		s = ft_strdup("(null)");
-	if (hdr->subspec_width && hdr->width > ft_strlen(s))
+	else
+		s = ft_strdup(s);
+	len = ft_strlen(s);
+	if (hdr->subspec_width && hdr->width > len)
 	{
 		ft_sub_specifier_minus(hdr, s);
 		fmt->length += write(1, hdr->width_string, hdr->width);
 		free(hdr->width_string);
 	}
 	else
-	{
-		fmt->length += write(1, s, ft_strlen(s));
-	}
+		fmt->length += write(1, s, len);
 	free(s);
 	fmt->i++;
 }
