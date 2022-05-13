2eb071d8b2b25e3951c3aa898a7abd2db330a121
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-05-13 23:05:11 +0200

    feat: add %-d

--- src/ft_placeholder.c
+++ src/ft_placeholder.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 23:29:44 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/05/13 15:23:06 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/05/13 22:57:59 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	ft_placeholder(t_format *fmt)
 	else if (fmt->format[fmt->i] == 'p')
 		ft_specifier_p(fmt, hdr);
 	else if (fmt->format[fmt->i] == 'd' || fmt->format[fmt->i] == 'i')
-		ft_specifier_d_i(fmt);
+		ft_specifier_d_i(fmt, hdr);
 	else if (fmt->format[fmt->i] == 'u')
 		ft_specifier_u(fmt);
 	else if (fmt->format[fmt->i] == 'x')
--- src/ft_specifier_d_i.c
+++ src/ft_specifier_d_i.c
@@ -6,20 +6,29 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 00:27:03 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/05/13 00:28:01 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/05/13 23:01:41 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-void	ft_specifier_d_i(t_format *fmt)
+void	ft_specifier_d_i(t_format *fmt, t_holder *hdr)
 {
 	char	*s;
 	int		d;
+	size_t	len;
 
 	d = (int)va_arg(fmt->ap, int);
 	s = ft_itoa(d);
-	fmt->length += write(1, s, ft_strlen(s));
-	free(s);
+	len = ft_strlen(s);
+	if (hdr->subspec_width && hdr->width > len)
+	{
+		ft_sub_specifier_minus(hdr, s);
+		fmt->length += write(1, hdr->width_string, hdr->width);
+		free(hdr->width_string);
+	}
+	else
+		fmt->length += write(1, s, len);
 	fmt->i++;
+	free(s);
 }
