8c3afd516509d87af6b81a4c1095554c04246e7e
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-05-16 17:23:57 +0200

    refactor: removed ft_strlen

--- src/ft_specifier_c.c
+++ src/ft_specifier_c.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 00:18:36 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/05/16 14:39:25 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/05/16 17:21:35 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,15 @@ void	ft_specifier_c(t_format *fmt, t_holder *hdr)
 	char	*s;
 
 	c = (char)va_arg(fmt->ap, int);
-	if (hdr->subspec_width)
+	if (hdr->subspec_width && hdr->width > 1)
 	{
 		s = ft_strdup("");
 		ft_subspec_justify(&s, hdr);
-		if (ft_strlen(s) > 0)
-		{
-			if (hdr->flag_minus)
-				s[0] = c;
-			else
-				s[hdr->width - 1] = c;
-			fmt->length += write(1, s, hdr->width);
-		}
+		if (hdr->flag_minus)
+			s[0] = c;
+		else
+			s[hdr->width - 1] = c;
+		fmt->length += write(1, s, hdr->width);
 		free(s);
 	}
 	else
--- tests/src/submix_test.c
+++ tests/src/submix_test.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 05:10:14 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/05/16 05:18:32 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/05/16 17:16:25 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 char	*submix_test(int (*fn_print)(const char *format, ...))
 {
-	char	*len = alloc_len(3);
+	char	*len = alloc_len(4);
 	int		i = 0;
+	len[i++] = fn_print("(%0c)\n", 'a'); fn_print("\n");
 	len[i++] = fn_print("(%01.d)\n", 0); fn_print("\n");
 	len[i++] = fn_print("(%01.x)\n", 0); fn_print("\n");
 	return (len);
