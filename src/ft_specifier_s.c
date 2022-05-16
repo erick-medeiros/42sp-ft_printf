ae112dd82209cceb7d018a8ac4ce75e9473800f5
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-05-16 03:29:46 +0200

    feat: %(dot)(s)

--- src/ft_specifier_s.c
+++ src/ft_specifier_s.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 00:23:47 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/05/16 00:26:12 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/05/16 03:28:49 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,52 +16,25 @@ void	ft_specifier_s(t_format *fmt, t_holder *hdr)
 {
 	char		*s;
 	char		*sn;
-	size_t		len;
 
 	sn = va_arg(fmt->ap, char *);
 	if (sn == NULL)
 		s = ft_strdup("(null)");
 	else
 		s = ft_strdup(sn);
-	len = ft_strlen(s);
 	hdr->flag_zero = 0;
-	if (hdr->subspec_dot)
-	{
-		if (sn != NULL)
-		{
-			if (hdr->subspec_width && !hdr->subspec_precision)
-			{
-				hdr->width = hdr->precision;
-				ft_strupd(&s, ft_substr(s, 0, hdr->width));
-				ft_subspec_buffer(hdr);
-			}
-			else if (!hdr->subspec_width && hdr->subspec_precision)
-			{
-				ft_strupd(&s, ft_substr(s, 0, hdr->width));
-				hdr->width = ft_strlen(s);
-				ft_subspec_buffer(hdr);
-				ft_subspec_justify(hdr, s);
-			}
-			else
-			{
-				ft_strupd(&s, ft_substr(s, 0, hdr->width));
-				hdr->width = hdr->precision;
-				ft_subspec_buffer(hdr);
-				ft_subspec_justify(hdr, s);
-			}
-			fmt->length += write(1, hdr->buffer, hdr->width);
-		}
-		else if (sn == NULL && hdr->width >= len)
-			fmt->length += write(1, s, len);
-	}
-	else if (hdr->subspec_width && hdr->width > len)
+	if (hdr->subspec_precision && (sn || hdr->precision >= 6))
+		ft_strupd(&s, ft_substr(s, 0, hdr->precision));
+	else if (hdr->subspec_precision)
+		ft_strupd(&s, ft_strdup(""));
+	if (hdr->subspec_width && hdr->width > ft_strlen(s))
 	{
 		ft_subspec_buffer(hdr);
 		ft_subspec_justify(hdr, s);
 		fmt->length += write(1, hdr->buffer, hdr->width);
 	}
 	else
-		fmt->length += write(1, s, len);
+		fmt->length += write(1, s, ft_strlen(s));
 	fmt->i++;
 	free(s);
 }
