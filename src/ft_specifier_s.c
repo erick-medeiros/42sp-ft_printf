ea36b8e060b152b9eb148a6ab08b1de77c20e4ac
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-05-16 16:24:49 +0200

    refactor: organized

--- src/ft_specifier_s.c
+++ src/ft_specifier_s.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 00:23:47 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/05/16 15:41:39 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/05/16 16:23:19 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,19 @@ void	ft_specifier_s(t_format *fmt, t_holder *hdr)
 	char		*s;
 	char		*sn;
 
-	sn = va_arg(fmt->ap, char *);
+	sn = (char *)va_arg(fmt->ap, char *);
+	hdr->flag_zero = 0;
 	if (sn == NULL)
 		s = ft_strdup("(null)");
 	else
 		s = ft_strdup(sn);
-	hdr->flag_zero = 0;
-	if (hdr->subspec_precision && (sn || hdr->precision >= 6))
-		ft_strupd(&s, ft_substr(s, 0, hdr->precision));
-	else if (hdr->subspec_precision)
-		ft_strupd(&s, ft_strdup(""));
+	if (hdr->subspec_precision)
+	{
+		if (sn || hdr->precision >= 6)
+			ft_strupd(&s, ft_substr(s, 0, hdr->precision));
+		else
+			ft_strupd(&s, ft_strdup(""));
+	}
 	hdr->length = ft_strlen(s);
 	if (hdr->subspec_width && hdr->width > hdr->length)
 		ft_subspec_justify(&s, hdr);
