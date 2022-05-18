4a1bd67b4adf4418a0fa285c4e47c667bb5b6a79
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-05-18 03:13:33 +0200

    fix: numbersign and precision in %x

--- src/ft_specifier_ui.c
+++ src/ft_specifier_ui.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 00:33:05 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/05/18 01:07:07 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/05/18 03:12:41 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ void	ft_specifier_ui(t_format *fmt, t_holder *hdr, char *prefix, char *base)
 	s = ft_uitoa_base(ui, base);
 	if (hdr->flag_minus)
 		hdr->flag_zero = 0;
-	if (hdr->flag_numbersign && ui > 0)
-		ft_strupd(&s, ft_strjoin(prefix, s));
 	if (hdr->subspec_precision)
 	{
 		if (hdr->precision == 0 && ui == 0)
@@ -30,6 +28,8 @@ void	ft_specifier_ui(t_format *fmt, t_holder *hdr, char *prefix, char *base)
 		else
 			ft_subspec_minimum_number(&s, hdr->precision);
 	}
+	if (hdr->flag_numbersign && ui > 0)
+		ft_strupd(&s, ft_strjoin(prefix, s));
 	hdr->length = ft_strlen(s);
 	if (hdr->subspec_width && hdr->width > hdr->length)
 		ft_subspec_justify(&s, hdr);
